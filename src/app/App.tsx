5cc640891fe107444b00ed400c6b94ce541f231b
YannickSendrey <yannicksendrey@gmail.com>
2023-10-15 14:16:53 +0200

    main menu
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-import { GameBoard } from './components/GameBoard';
-import { Menu } from './components/Menu';
+import { GameBoard } from '../components/GameBoard';
+import { Menu } from '../components/Menu';
 import React, { useState } from 'react';
 
 function App() {
