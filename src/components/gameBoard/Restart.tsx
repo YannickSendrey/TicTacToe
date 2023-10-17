cb55a910f1b184199a40fe8e4a86db733ba3fe79
YannickSendrey <yannicksendrey@gmail.com>
2023-10-17 11:18:16 +0200

    header styling
    Signed-off-by: YannickSendrey <yannicksendrey@gmail.com>

--- src/components/gameBoard/Restart.tsx
+++ src/components/gameBoard/Restart.tsx
@@ -3,7 +3,9 @@ import '../../css/gameBoard.css';
 export const Restart = () => {
     return (
         <div className='restart-container'>
-            <img src="/assets/icon-restart.svg" alt="restart game logo" className="restart"></img>
+            <div className='restart'>
+                <img src="/assets/icon-restart.svg" alt="restart game logo" className="restart-logo"></img>
+            </div>
         </div>
     );
 }
--- src/components/gameBoard/Turn.tsx
+++ src/components/gameBoard/Turn.tsx
@@ -3,8 +3,10 @@ import '../../css/gameBoard.css';
 export const Turn = () => {
     return (
         <div className="turn-container">
-            <div className="turn-logo"></div>
-            <h2>TURN</h2>
+            <div className='turn'>
+                <img src="/assets/icon-x.svg" alt="player turn letter" className='turn-logo'/>
+                <h2>TURN</h2>
+            </div>
         </div>
     )
 }
